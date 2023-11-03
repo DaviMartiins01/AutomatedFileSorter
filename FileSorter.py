5fd11d51ddc41e192210f62c7f09bae9ce9cb2b7
Davi Martins <davibarroso12399@gmail.com>
2023-11-02 21:14:55 -0300

    criando as pastas caso não existam

--- FileSorter.py
+++ FileSorter.py
@@ -1,2 +1,9 @@
 import os, shutil
-path = r"C:\Users\davib\Documents\Mess"
+
+path = r"C:/Users/davib/Documents/Mess/"
+
+folder_names = ["ArquivosCSV", "ArquivosPNG", "ArquivosTXT"]
+
+for loop in range(0, 3):
+	if not os.path.exists(path + folder_names[loop]):
+		os.makedirs(path + folder_names[loop])
